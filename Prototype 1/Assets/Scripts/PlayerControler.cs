506d4de164d946e21327e452bf41617616e6e119
Rebekah Lauret <b3k.laur3t@gmail.com>
2019-09-23 16:44:47 -0600

    Had an issue with the first repo.  Created a second one where I added: -Prototype 1 completed -Challange 1

---
+++ Prototype 1/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+ 
+ {   
+    //Private Variables
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput);
+        // Rotates the car on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}

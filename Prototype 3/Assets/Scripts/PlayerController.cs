c36f487798d0f0c08365d8189cabd77a12603b32
Rebekah Lauret <b3k.laur3t@gmail.com>
2019-10-21 11:10:31 -0600

    Added GameOver tag
    Added game over tag and 
    added ground and obstacle tag in item inspectors

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +27,15 @@ public class PlayerController : MonoBehaviour
          isOnGround = false;
         }}
     private void OnCollisionEnter(Collision collision){
-        isOnGround = true;
+       
+
+        if(collision.gameObject.CompareTag("Ground")){
+             isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obsticle")){
+            Debug.Log("Game Over");
+
+        }
     }
     
 }

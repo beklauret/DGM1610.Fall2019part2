658e9b54eccf9269946f85cdfe478d0da623a380
Rebekah Lauret <b3k.laur3t@gmail.com>
2019-10-28 17:09:06 -0600

    Particles and sounds
    Added particle effects
    began adding sound - background (ambient)

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -24,22 +26,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround){
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){
          playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
          isOnGround = false;
          playerAnim.SetTrigger("Jump_trig");
+         dirtParticle.Stop();
         }}
-    private void OnCollisionEnter(Collision collision){
-       
-
-        if(collision.gameObject.CompareTag("Ground")){
+    private void OnCollisionEnter(Collision other){
+        if(other.gameObject.CompareTag("Ground")){
              isOnGround = true;
+             dirtParticle.Play();
         }
-        else if(collision.gameObject.CompareTag("Obstacle")){
+        else if(other.gameObject.CompareTag("Obstacle")){
             Debug.Log("Game Over");
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
     

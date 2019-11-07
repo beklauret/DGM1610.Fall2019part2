e3368bb37efa8d0faaa07f32220c0bbcefa86863
Rebekah Lauret <b3k.laur3t@gmail.com>
2019-11-06 17:08:13 -0700

    Enemy and powerup
    Added Enemy
    Created spawn manager
    added powerup
    added destroy powerup as the player impacts it

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Powerup")){
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+
+    private void OnCollisionEnter(Collection collision){
+        if (collision.CompareTag("Enemy") && hasPowerup){
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.postion - transform.position);
+            Debug.Log("Player collider with " + collision.gameObject + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impuls);
+        }
+    }
+    }
+}
